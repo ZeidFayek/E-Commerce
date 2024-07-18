ff74aa96446348c63f934b03b29f5d8429e7e284
ZeidFayek <zeyadfayek012@gmail.com>
2024-07-18 03:38:36 +0300

    upload E-commerce

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -10,51 +10,51 @@ import { AuthService } from 'src/app/services/auth.service';
 })
 export class RegisterComponent {
 
-  constructor(private _AuthService:AuthService , private _Router:Router){}
+  constructor(private _AuthService: AuthService, private _Router: Router) { }
 
-  isLoading:boolean = false;
-  errMessage:String = '';
+  isLoading: boolean = false;
+  errMessage: String = '';
 
-  registerForm:FormGroup = new FormGroup({
-    name:new FormControl('' , [Validators.required , Validators.minLength(3) , Validators.maxLength(20)]),
-    email:new FormControl('' , [Validators.required , Validators.email]),
-    password:new FormControl('' , [Validators.required , Validators.pattern(/^\w{6,}$/)]),
-    rePassword:new FormControl(''),
-    phone:new FormControl('' , [Validators.required , Validators.pattern(/^01[0125][0-9]{8}$/)])
-  } , { validators:[this.confirmPassword] } as FormControlOptions)
+  registerForm: FormGroup = new FormGroup({
+    name: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(20)]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.pattern(/^\w{6,}$/)]),
+    rePassword: new FormControl(''),
+    phone: new FormControl('', [Validators.required, Validators.pattern(/^01[0125][0-9]{8}$/)])
+  }, { validators: [this.confirmPassword] } as FormControlOptions)
 
-  confirmPassword(group:FormGroup):void{
+  confirmPassword(group: FormGroup): void {
     const password = group.get('password');
     const rePassword = group.get('rePassword');
 
-    if( rePassword?.value == '' ){
-      rePassword.setErrors({ required:true })
+    if (rePassword?.value == '') {
+      rePassword.setErrors({ required: true })
     }
-    else if( password?.value != rePassword?.value ){
-      rePassword?.setErrors({ mismatch:true })
+    else if (password?.value != rePassword?.value) {
+      rePassword?.setErrors({ mismatch: true })
     }
   }
 
 
-  handleRegister():void{ 
-    
+  handleRegister(): void {
+
     this.isLoading = true
 
-    if(this.registerForm.valid){
-      this._AuthService.registerForm (this.registerForm.value).subscribe({
-        next:(response)=>{
+    if (this.registerForm.valid) {
+      this._AuthService.registerForm(this.registerForm.value).subscribe({
+        next: (response) => {
           console.log(response)
-          if(response.message === 'success'){
+          if (response.message === 'success') {
             this._Router.navigate(['/login'])
           }
           this.isLoading = false
         },
 
-        error:(err)=>{
+        error: (err) => {
           this.isLoading = false
-          this.errMessage = err.error.message          
+          this.errMessage = err.error.message
         }
       })
-    } 
+    }
   }
 }
--- src/app/pipes/search.pipe.ts
+++ src/app/pipes/search.pipe.ts
@@ -5,8 +5,8 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class SearchPipe implements PipeTransform {
 
-  transform( products:any[] , term:string ): any[] {
-    return products.filter( (item) => item.title.toLowerCase().includes(term.toLowerCase()) );
+  transform(products: any[], term: string): any[] {
+    return products.filter((item) => item.title.toLowerCase().includes(term.toLowerCase()));
   }
 
 }
